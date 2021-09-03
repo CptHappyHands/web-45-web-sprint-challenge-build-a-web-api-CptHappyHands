dfb35159c8fd360cc5a34e63ccd3f2ec967ff5a9
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-03 08:55:18 -0700

    server conected to local port

--- api/server.js
+++ api/server.js
@@ -6,4 +6,8 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+server.get("/", (req, res) => {
+    res.send(`<h1>The Waystone Inn lay in silence, and it was a silence of three parts...</h1>`)
+})
+
 module.exports = server;
