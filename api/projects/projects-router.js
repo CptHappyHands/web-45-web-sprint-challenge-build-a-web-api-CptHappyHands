f270eb5596d83d3143e02f271de6ed22d0fa35dd
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-03 09:19:42 -0700

    starting endpoints

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
 // Write your "projects" router here!
+const express = require('express')
+const Project = require("./projects-model")
+const router = express.Router()
+
+router.get("/projects", (req, res, next) => {
+
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        customMessage: "Nothing good hapening in posts",
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const server = express();
+const projectsRouter = require("./projects/projects-router")
 
-
+server.use(express.json())
+server.use("/api", projectsRouter)
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
