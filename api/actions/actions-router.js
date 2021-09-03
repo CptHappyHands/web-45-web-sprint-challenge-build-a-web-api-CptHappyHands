a04bed369b5ab711a5016924a9274d3720bdaeec
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-03 11:53:53 -0700

    27/27 passing!

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -26,6 +26,69 @@ router.get("/:id", validateActionId, async (req, res, next) => {
     }
 })
 
+
+router.post("/", (req, res, next) => {
+    const newAction = req.body
+    if(!newAction.description || !newAction.notes || !newAction.project_id) {
+        res.status(400).json({ message: "text input required" })
+    } else {
+        Action.insert(newAction)
+            .then(({ id }) => {
+                return Action.get(id)
+            })
+            .then((action) => {
+                res.status(201).json(action)
+            })
+            .catch((err) => {
+                next(err)
+            })
+    }
+})
+
+router.put("/:id", async (req, res, next) => {
+    const updateAction = req.body
+    if(!updateAction.description || !updateAction.notes || !updateAction.project_id || updateAction.completed == req.completed) {
+        res.status(400).json({ message: "text input required" })
+    } else {
+        Action.get(req.params.id)
+            .then((action) => {
+                if(!action) {
+                    res.status(404).json({ message: "This ID does not exist"})
+                } else {
+                    return Action.update(req.params.id, req.body)
+                }
+            })
+            .then((data) => {
+                if (data) {
+                    return Action.get(req.params.id, req.body)
+                }
+            })
+            .then((moreData) => {
+                if (moreData) {
+                    res.json(moreData)
+                }
+            })
+            .catch((err) => {
+                next(err)
+            })
+    }
+})
+
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const actionData = await Action.get(req.params.id)
+        if(!actionData) {
+            res.status(404).json({ message: "Cannot delete because this ID does not exist"})
+        } else {
+            await Action.remove(req.params.id)
+            res.status(200).json(actionData)
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
 router.use((err, req, res, next) => {
     res.status(err.status || 500).json({
         customMessage: "Nothing good hapening in posts",
@@ -33,7 +96,6 @@ router.use((err, req, res, next) => {
         stack: err.stack
     })
 })
-
 
 
 module.exports = router
